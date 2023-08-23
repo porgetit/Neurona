fa8e0e17a3ad760da94fa0817a34d8012ec75a74
Porgetit <kevin.esguerra@utp.edu.co>
2023-08-23 00:16:18 -0500

    Crear proyecto Reto 3

---
+++ Ejercicios/Reto_3/src/main/java/com/mycompany/reto_3/Reto_3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reto_3;
+
+/**
+ *
+ * @author athirtro
+ */
+public class Reto_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
